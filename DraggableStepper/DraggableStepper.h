0337653b14d281d1406093b41659cb28e50c6515
Hector Ghinaglia <hghinaglia@gmail.com>
2017-09-06 18:23:10 -0300

    Initial Commit

---
+++ DraggableStepper/DraggableStepper.h
@@ -0,0 +1,19 @@
+//
+//  DraggableStepper.h
+//  DraggableStepper
+//
+//  Created by Hector Ghinaglia on 9/6/17.
+//  Copyright © 2017 Hector Ghinaglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DraggableStepper.
+FOUNDATION_EXPORT double DraggableStepperVersionNumber;
+
+//! Project version string for DraggableStepper.
+FOUNDATION_EXPORT const unsigned char DraggableStepperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DraggableStepper/PublicHeader.h>
+
+
